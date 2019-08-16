7ee065d9042a2464c8c6e37e5123a037ceee21fb
sw3196 <sw3196@columbia.edu>
2019-08-16 18:29:57 -0400

    changed app text

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           {/* Edit <code>src/App.js</code> and save to reload. */}
-          hi there hahaha
+          Made changes on the feature branch.
         </p>
         <a
           className="App-link"
